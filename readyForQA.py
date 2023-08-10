004d15bb6c8df5d87b733d586b809d5a037b642c
Zeeshan <zeeshanmedlari@gmail.com>
2023-08-10 11:25:33 +0530

    Improvements on readyForQA file

--- readyForQA.py
+++ readyForQA.py
@@ -1,15 +1,18 @@
 import requests
 import sys
 import getopt
-#from kubernetes import client, config
+from decouple import config
+from kubernetes import client
 
-'''def check_kubernetes_deployment(jira_id):
+kube_config_path = "/home/prod-usa.jaspersystem.com/zhyderme/sanity3"
+
+def check_kubernetes_deployment(jira_id, kube_config_path):
     # Load the Kubernetes configuration
-    config.load_kube_config()
+    config.load_kube_config(kube_config_path)
 
     # Replace 'your_namespace' and 'your_deployment_name' with your actual namespace
-    namespace = "your_namespace"
-    deployment_name = "your_deployment_name"
+    namespace = "cdr"
+    deployment_name = "cdr-mediation-parser"
 
     # Initialize the Kubernetes API client
     apps_v1_api = client.AppsV1Api()
@@ -27,43 +30,44 @@ import getopt
             return False
     except client.exceptions.ApiException as e:
         print(f"Error occurred while checking the deployment for {jira_id}: {e}")
-        return False'''
+        return False
 
 def update_jira_status(jira_id):
-    jira_credentials = "zeeshan:'Zameera@123!'"
 
-    read_cmd = f"https://zeeshanmedlari.atlassian.net/jira/rest/api/latest/issue/{jira_id}?fields=status"
+    read_url = f"https://zeeshanmedlari.atlassian.net/jira/rest/api/latest/issue/{jira_id}?fields=status"
+    headers = {
+        "Content_Type" : "application/json"
+    }
     
     # Check current status of ticket
-    response = requests.get(read_cmd, auth=jira_credentials)
+    response = requests.get(read_url, headers=headers, auth=(config("JIRA_USERNAME"), config("JIRA_PASSWORD")))
+
     if response.status_code == 200:
         status = response.json()
-    else:
-        print(f"Failed to fetch the current status for {jira_id}. Exiting.")
-        sys.exit(1)
 
-    if "fields" in status and "status" in status["fields"]:
-        current_status = status["fields"]["status"]["name"]
-    else:
-        print("Unable to fetch the current status. Exiting.")
-        sys.exit(1)
+        if "fields" in status and "status" in status["fields"]:
+            current_status = status["fields"]["status"]["name"]
+        else:
+            print("Unable to fetch the current status. Exiting.")
+            sys.exit(1)
 
-    if current_status != "Fixed":
-        print(f"Status is not Fixed: Current Status is {current_status}. Exiting.")
-        sys.exit(0)
+        if current_status != "Fixed":
+            print(f"Status is not Fixed: Current Status is {current_status}. Exiting.")
+            sys.exit(0)
 
-    print("Bug is fixed - let's move it to ReadyForQA")
+        print("Bug is fixed - let's move it to ReadyForQA")
 
-    update_cmd = f"https://zeeshanmedlari.atlassian.net/jira/rest/api/latest/issue/{jira_id}/transitions?expand=transitions.fields"
-    data = {"transition": {"id": "711"}}
+        update_url = f"https://zeeshanmedlari.atlassian.net/jira/rest/api/latest/issue/{jira_id}/transitions?expand=transitions.fields"
+        update_data = {"transition": {"id": "711"}}
 
-    # Move ticket to ReadyForQA
-    response = requests.post(update_cmd, auth=jira_credentials, json=data)
-    if response.status_code == 204:
-        print(f"Status of {jira_id} changed to ReadyForQA")
-    else:
-        print(f"ERROR: Status of {jira_id} could not be moved to ReadyForQA. Exiting.")
-        sys.exit(1)
+        # Move ticket to ReadyForQA
+        update_response = requests.post(update_url, json=update_data, headers=headers, auth=(config("JIRA_USERNAME"), config("JIRA_PASSWORD")))
+
+        if update_response.status_code == 204:
+            print(f"Status of {jira_id} changed to ReadyForQA")
+        else:
+            print(f"ERROR: Status of {jira_id} could not be moved to ReadyForQA. Exiting.")
+            sys.exit(1)
 
 def main(argv):
     jira_id = ""
@@ -91,12 +95,12 @@ def main(argv):
     if jira_id in ["CC-120222", "CC-104616", "CC-39782", "CC-0000", "CC-00000", "CC-000000"]:
         sys.exit()
 
-    '''# Check the Kubernetes deployment status
+    # Check the Kubernetes deployment status
     if not check_kubernetes_deployment(jira_id):
-        sys.exit()'''
+        sys.exit()
 
     # Update the JIRA status to "ReadyForQA"
     update_jira_status(jira_id)
 
 if __name__ == "__main__":
-    (sys.argv[1:])
+    main(sys.argv[1:])
